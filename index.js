725886485b39a8247d94b7901a8c72ba043851d0
Bliss <blissmaake140@gmail.com>
2022-11-30 17:03:54 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arrDrivers, matchDrivers){
+return arrDrivers.filter( driver => driver.toLowerCase() === matchDrivers.toLowerCase());
+
+}
+
+function fuzzyMatch (arrDrivers, firstLetter) {
+    return arrDrivers.filter (letters => letters.startsWith(firstLetter))
+}
+
+function matchName (arrDrivers, nameMatch) {
+    return arrDrivers.filter (name => name.name === nameMatch)
+}
